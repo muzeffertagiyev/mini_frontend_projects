5fe6c5da3210d6dc28aa2ee1087827f36ddb5483
muzeffertagiyev <muzaffar.taghiyev@gmail.com>
2025-05-04 23:55:46 +0300

    movies were added to DOM

--- movie_app/script.js
+++ movie_app/script.js
@@ -3,7 +3,7 @@ const API_URL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMAGE_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API= 'https://api.themoviedb.org/3/search/movie?api_key=e426388127dc3ada71fcd97938f7c904&query="'
 
-
+const main = document.getElementById("main")
 const form = document.getElementById("form")
 const search = document.getElementById("search")
 // GET INITIAL MOVIES
@@ -17,7 +17,7 @@ async function getMovies(url){
             throw new Error(res.status)
         }
         const data = await res.json()
-        console.log(data.results);
+        showMovies(data.results)
         
     }catch(error){
         document.querySelector("body").innerText = error
@@ -38,3 +38,41 @@ form.addEventListener("submit",(e)=>{
     }
 })
 
+
+
+function showMovies(movies){
+    main.innerHTML = ''
+
+    movies.forEach((movie)=>{
+        const {title,poster_path,vote_average, overview} = movie
+        const movieEl = document.createElement("div")
+        movieEl.classList.add("movie")
+
+        movieEl.innerHTML =`
+                <img src="${IMAGE_PATH+poster_path}" alt="${title}">
+                <div class="movie-info">
+                    <h3 class="title">
+                        ${title}
+                    </h3>
+                    <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+                </div>
+                <div class="overview">
+                    <h3>Overview</h3>
+                    ${overview}
+                </div>
+        `
+        main.appendChild(movieEl)
+
+    })
+}
+
+
+function getClassByRate(vote){
+    if(vote>=8){
+        return 'green'
+    }else if(vote>=5){
+        return 'orange'
+    }else{
+        return 'red'
+    }
+}
