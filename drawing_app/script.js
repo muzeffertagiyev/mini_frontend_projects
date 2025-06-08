b69570e6f77037c897c5dad853fa0140377e4674
muzeffertagiyev <muzaffar.taghiyev@gmail.com>
2025-06-08 19:39:56 +0300

    project finished

--- drawing_app/script.js
+++ drawing_app/script.js
@@ -1,8 +1,15 @@
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext('2d')
 
+const increaseBtn = document.getElementById("increase")
+const decreaseBtn = document.getElementById("decrease")
+const sizeEl = document.getElementById("size")
+const colorEl = document.getElementById("color")
+const clearBtn = document.getElementById("clear")
 
-let size = 20
+
+
+let size = 10
 let isPressed = false
 let color = 'black'
 
@@ -52,3 +59,30 @@ function drawLine(x1, y1, x2, y2){
     ctx.lineWidth = size * 2
     ctx.stroke()
 }
+
+function updateSizeOnScreen(){
+    sizeEl.innerText = size
+}
+
+increaseBtn.addEventListener("click",()=>{
+    size += 5
+
+    if(size>50){
+        size = 50
+    }
+
+    updateSizeOnScreen()
+} )
+
+decreaseBtn.addEventListener("click",()=>{
+    size -= 5
+
+    if(size<5){
+        size = 5
+    }
+
+    updateSizeOnScreen()
+})
+
+colorEl.addEventListener("change",(e)=>color = e.target.value)
+clearBtn.addEventListener("click", () => ctx.clearRect(0, 0, canvas.width, canvas.height))
